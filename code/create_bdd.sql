3d35742b18c17556b8dcf9a67f0a827afbe289ca
Doufani <131384700+Doufani85@users.noreply.github.com>
2024-01-22 22:12:52 +0100

    create databse and tables

---
+++ code/create_bdd.sql
@@ -0,0 +1,105 @@
+Create DATABASE  IF NOT EXISTS bdd_projet;
+
+DROP TABLE IF EXISTS bdd_projet.artist;
+CREATE EXTERNAL TABLE bdd_projet.artist (
+  artist_id INT,
+  full_name STRING,
+  first_name STRING,
+  middle_names STRING,
+  last_name STRING,
+  nationality STRING,
+  style STRING,
+  birth date,
+  death date
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/artist';
+
+DROP TABLE IF EXISTS bdd_projet.canvas_size;
+CREATE EXTERNAL TABLE bdd_projet.anvas_size (
+  size_id INT,
+  width INT,
+  height INT,
+  label string
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/canvas_size';
+
+DROP TABLE IF EXISTS bdd_projet.image_link;
+CREATE EXTERNAL TABLE bdd_projet.image_link (
+  work_id INT,
+  url STRING,
+  thumbnail_small_url STRING,
+  thumbnail_large_url STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/image_link';
+
+DROP TABLE IF EXISTS bdd_projet.meseum;
+CREATE EXTERNAL TABLE bdd_projet.meseum (
+  museum_id INT,
+  Name STRING,
+  address STRING,
+  city STRING,
+  state STRING,
+  postal INT,
+  country STRING,
+  phone STRING,
+  url STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/meseum';
+
+DROP TABLE IF EXISTS bdd_projet.meseum_hours;
+CREATE EXTERNAL TABLE bdd_projet.meseum_hours (
+  museum_id INT,
+  day STRING, 
+  open STRING, 
+  close STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/meseum_hours';
+
+DROP TABLE IF EXISTS bdd_projet.product_size;
+CREATE EXTERNAL TABLE bdd_projet.product_size (
+  work_id INT,
+  size_id INT,
+  sale_price INT,
+  regular_price INT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/product_size';
+
+DROP TABLE IF EXISTS bdd_projet.subject;
+CREATE EXTERNAL TABLE bdd_projet.subject (
+  work_id INT,
+  subject STRING
+
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/subject';
+
+DROP TABLE IF EXISTS bdd_projet.work;
+CREATE EXTERNAL TABLE bdd_projet.work (
+  work_id INT,
+  name STRING,
+  artist_id INT,
+  style STRING,
+  museum_id INT
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LOCATION '/user/data/work';
+
+
+
+
+
+
